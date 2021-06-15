3c4ab95437ba797801548e49252c7045c3b98eda
Shlok Sethia <81409900+shlok-sethia@users.noreply.github.com>
2021-06-15 10:47:17 +0530

    fix bugs and zoom preference

--- HDBScan_cluster_and_heatmap.py
+++ HDBScan_cluster_and_heatmap.py
@@ -17,7 +17,7 @@ def get_points_inside_box(df, box):
 px.set_mapbox_access_token('access_token')
 
 
-def get_cluster_map(locations, bounding_box):
+def get_cluster_map(locations, bounding_box, zoom):
     loc_filtered = get_points_inside_box(locations, bounding_box)
     print(loc_filtered.shape)
     clusterer = hdbscan.HDBSCAN(min_cluster_size=10, metric='haversine')
@@ -34,17 +34,17 @@ def get_cluster_map(locations, bounding_box):
     print(cluster.head())
 
     fig = px.scatter_mapbox(cluster.loc[cluster['cluster'] > -1], lat="Latitude", lon="Longitude", size='Count',
-                            color='Count', color_continuous_scale=px.colors.sequential.Aggrnyl, zoom=10)
+                            color='Count', color_continuous_scale=px.colors.sequential.Aggrnyl, zoom=zoom)
 
     return fig
 
 florida_box = [(-88,24), (-76,30)]
 miami_box = [(-81.22,25.5), (-79.8,26.2)]
 
-out_map = get_cluster_map(loc_df, florida_box)
+out_map = get_cluster_map(loc_df, florida_box, 6)
 out_map.show()
 plotly.offline.plot(out_map, filename='florida_heatmap.html')
 
-out_map = get_cluster_map(loc_df, miami_box)
+out_map = get_cluster_map(loc_df, miami_box, 9)
 out_map.show()
 plotly.offline.plot(out_map, filename='miami_heatmap.html')
